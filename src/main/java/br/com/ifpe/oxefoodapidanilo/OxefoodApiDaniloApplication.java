42a02ed3d4f8d2d51ba66e3909cd8ef0b45a9487
aluno@aluno <aluno@ifpe.com>
2023-04-03 19:39:06 -0300

    03/04

---
+++ src/main/java/br/com/ifpe/oxefoodapidanilo/OxefoodApiDaniloApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapidanilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiDaniloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiDaniloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodapidanilo/OxefoodApiDaniloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapidanilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiDaniloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
