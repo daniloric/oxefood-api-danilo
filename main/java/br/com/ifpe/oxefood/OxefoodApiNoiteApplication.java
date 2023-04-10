8c012e45058eae32552fbbc5061b0640c826d2de
aluno@aluno <aluno@ifpe.com>
2023-04-10 20:10:42 -0300

    10/04

--- main/java/br/com/ifpe/oxefood/OxefoodApiNoiteApplication.java
+++ main/java/br/com/ifpe/oxefood/OxefoodApiNoiteApplication.java
@@ -1,13 +1,13 @@
-package br.com.ifpe.oxefoodapidanilo;
+package br.com.ifpe.oxefood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OxefoodApiDaniloApplication {
+public class OxefoodApiNoiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OxefoodApiDaniloApplication.class, args);
+		SpringApplication.run(OxefoodApiNoiteApplication.class, args);
 	}
 
 }
--- main/java/br/com/ifpe/oxefood/util/exception/PreenchimentoException.java
+++ main/java/br/com/ifpe/oxefood/util/exception/PreenchimentoException.java
@@ -20,4 +20,3 @@ public class PreenchimentoException extends RuntimeException {
     }
 
 }
-
